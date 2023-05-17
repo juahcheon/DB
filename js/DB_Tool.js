75236deb51c69d8409e57b04b27792180d2bc467
juahcheon <132863519+juahcheon@users.noreply.github.com>
2023-05-17 09:06:51 +0900

    study_0516

--- js/DB_Tool.js
+++ js/DB_Tool.js
@@ -2,17 +2,45 @@ $(function(){
 
     // list-group toggle
     var toggleBtn = $(".list_wrap li");
+    var pageBtn = $(".page-link");
     var toggleText = $(".list_list").find("li");
 
-    toggleText.hide();
 
+    toggleText.hide();
     toggleBtn.on("click", function(){
+        $(this).toggleClass("active");
         $(this).find("ul li").slideToggle();
     });
 
+    pageBtn.on("click", function(){
+        $(this).addClass("active");
+    });
+
+    pageBtn.on("mouseenter", function(){
+        $(this).removeClass("active");
+    });
 
 
+    var modalBtn = $(".delete .bi-trash");
+    modalBtn.on("click", function(){
+        $(".modal").show();
+    });
 
+    var btnClose = $(".btn-close");
+    var btnClose_02 = $(".btn-secondary")
+    btnClose.on("click", function(){
+        $(".modal").hide();
+    })
+
+    btnClose_02.on("click", function(){
+        $(".modal").hide();
+    })
+
+    $(".calender").flatpickr(
+        {
+            dateFormat: "Y-m-d H:i",
+        }
+    );
 
 
 
